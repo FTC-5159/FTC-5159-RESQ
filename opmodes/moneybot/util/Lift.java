df606c6955b4bea4aea771f9c8dbbc4388b846f4
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2015-11-23 15:35:33 -1000

    Add a gate and make Mexico pay for it

--- opmodes/moneybot/util/Lift.java
+++ opmodes/moneybot/util/Lift.java
@@ -3,21 +3,33 @@ package com.qualcomm.ftcrobotcontroller.opmodes.moneybot.util;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Lift
 {
 
     private DcMotor lift_motor;
+    private Servo gate;
+    
+    private final double NORMAL_POWER = 0.75;
+    
+    private final double DOWN_POSITION = 0;
+    private final double UP_POSITION = 0.5;
+    
 
     // 1 = full on
     // 0 = off
     // -1 = backwards
+    
     private int liftStatus = 0;
     private boolean dpad_cooldown = false;
+    private boolean y_cooldown = false;
 
-    public Lift(DcMotor lift_motor)
+    public Lift(DcMotor lift_motor, Servo gate)
     {
         this.lift_motor = lift_motor;
+        this.gate = gate;
+
     }
 
     public void runLift(Gamepad gamepad) {
@@ -33,11 +45,24 @@ public class Lift
             dpad_cooldown = false;
         }
 
-        lift_motor.setPower(liftStatus * 0.75);
+        lift_motor.setPower(liftStatus * NORMAL_POWER);
+
+        if (gamepad.y && !y_cooldown)
+        {
+            if (gate.getPosition() == UP_POSITION)
+                gate.setPosition(DOWN_POSITION);
+            else
+                gate.setPosition(UP_POSITION);
+            y_cooldown = true;
+
+        } else if (!gamepad.y && y_cooldown)
+            y_cooldown = false;
+
     }
 
     public void runLift(double power)
     {
-        lift_motor.setPower(power * 0.75);
+        lift_motor.setPower(power * NORMAL_POWER);
+
     }
 }
