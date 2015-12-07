473482d231b41bdb56722aa869fd548f5b3b4bbc
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2015-12-07 12:26:22 -1000

    Update Lift.java

--- opmodes/moneybot/util/Lift.java
+++ opmodes/moneybot/util/Lift.java
@@ -4,6 +4,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes.moneybot.util;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 public class Lift
 {
@@ -13,17 +14,14 @@ public class Lift
 
     private final double NORMAL_POWER = 0.75;
 
-    private final double DOWN_POSITION = 0;
+    private final double DOWN_POSITION = 1;
     private final double UP_POSITION = 0.5;
 
+
+    // false = down, true = up
+    private boolean gateStatus = false;
+
     private String status;
-
-
-    // 1 = full on
-    // 0 = off
-    // -1 = backwards
-
-
 
     private int liftStatus = 0;
     private boolean dpad_cooldown = false;
@@ -34,7 +32,15 @@ public class Lift
         this.lift_motor = lift_motor;
         this.gate = gate;
 
+        //gate.setPosition(DOWN_POSITION);
+
     }
+
+    public Lift(DcMotor lift_motor)
+    {
+        this.lift_motor = lift_motor;
+    }
+
 
     public void runLift(Gamepad gamepad) {
         if (!dpad_cooldown) {
@@ -49,15 +55,16 @@ public class Lift
             dpad_cooldown = false;
         }
 
-        lift_motor.setPower(liftStatus * NORMAL_POWER);
+        lift_motor.setPower(Range.clip(liftStatus * NORMAL_POWER, -1, 1));
 
         if (gamepad.y && !y_cooldown)
         {
-            if (gate.getPosition() == UP_POSITION)
+            if (gateStatus)
                 gate.setPosition(DOWN_POSITION);
             else
                 gate.setPosition(UP_POSITION);
             y_cooldown = true;
+            gateStatus = !gateStatus;
 
         } else if (!gamepad.y && y_cooldown)
             y_cooldown = false;
@@ -68,7 +75,7 @@ public class Lift
 
     public void runLift(int power)
     {
-        lift_motor.setPower(power * NORMAL_POWER);
+        lift_motor.setPower(Range.clip(power * NORMAL_POWER, -1, 1));
 
     }
 
